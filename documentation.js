62972913c93d8d0382680356924f20553c83ffeb
Chris Nasr <ouroboroscode@gmail.com>
2019-03-04 13:18:31 -0500

    - Implementing actual documentation - Added CSS for next/prev pagination

--- documentation.js
+++ documentation.js
@@ -1,7 +1,6 @@
 documentation = {
 	"platforms": [
 		{"key": "javascript", "title": "JavaScript"},
-		{"key": "php", "title": "PHP"},
 		{"key": "python", "title": "Python"}
 	],
 	"menu": [
@@ -22,14 +21,27 @@ documentation = {
 		]}
 	],
 	"pages": {
-		"intro": {"title": "Introduction", "sections": [
-			{"type": "paragraph", "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"type": "paragraph", "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
-		]},
-		"download": {"title": "Downloading", "sections": [
-			{"type": "paragraph", "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"type": "paragraph", "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
-		]},
+		"intro": {
+			"title": "Introduction",
+			"pagination": {
+				"next": "download"
+			},
+			"sections": [
+				{"type": "paragraph", "text": "Several years ago I was hired to by a company to re-write their cloud based software from scratch. The product relied heavily on document style data but version 1.0 had absolutely no validation in place so the DB was a mish-mash of different formats that were very hard to make sense of. I absolutely did not want version 2.0 to end up the same way, and I was also toying with the idea of validating data in both the browser as well as the server side using the exact same definition files, so I ended up writing a very simple piece of code in Python 2 / PHP / JavaScript which I called DataPoint. It worked out initially, but as I started to understand the scope of how complex one document could be, arrays, dynamic objects, points that could have two totally different but still valid types of data, the hack job that DataPoint became was so hard to use that it was almost as much trouble as not having validation. So in early 2016 I started spending my nights and weekends rewriting the entire thing as a self-contained pip/npm project that would take into account everything I had learned. FormatOC was born, and yes, we can all agree, I am terrible at naming things."},
+				{"type": "paragraph", "text": "The idea behind FormatOC was to be able to define complex sets of data using JSON that could be loaded in any programming language that could be validating in both true data values, as well as strings that come back from browser forms, and then once validated, could be converted to whatever the DB required. This means that if a particular service required an integer or a double, they could receive \"10\" or \"98.6\" and be just as happy as if they got 10 or 98.6."}
+			]
+		},
+		"download": {
+			"title": "Downloading",
+			"pagination": {
+				"prev": "intro",
+				"next": "install"
+			},
+			"sections": [
+				{"type": "paragraph", "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+				{"type": "paragraph", "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
+			]
+		},
 		"install": {"title": "Installing", "sections": [
 			{"type": "paragraph", "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
 			{"type": "paragraph", "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
